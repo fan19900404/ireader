c6c50189215bb1b3af68fe65ea824888c82f60b4
fan19900404 <125920969@qq.com>
2017-12-10 23:39:40 +0800

    网站主体，包括路由以及处理

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const config = require('./config');
+const http = require("http");
+const url = require("url");
+const qs = require("querystring");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const myurl = url.parse(req.url);
+  if (myurl.pathname === "/api/catalog") {
+    // 转换目录
+    const catalogUrl = qs.parse(myurl.query).url;
+    const host = url.parse(catalogUrl).host;
+    if(config[host]){
+      config[host].catalog(catalogUrl,res);
+    }else{
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html;charset=utf-8");
+      res.end(`暂时不支持${host}\n`);
+    }
+  } else if (myurl.pathname === "/api/info") {
+    // 转换详情
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html;charset=utf-8");
+    res.end(`${myurl.pathname}详情\n`);
+  } else {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World\n");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
