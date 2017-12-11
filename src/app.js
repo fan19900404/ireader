e6c05050cb0ac66e5d5e3a3ff909de9363e38beb
fan19900404 <125920969@qq.com>
2017-12-12 00:46:04 +0800

    读取html文件功能

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-const config = require('./config');
+const config = require("./config");
 const http = require("http");
 const url = require("url");
 const qs = require("querystring");
+const fs = require("fs");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -12,9 +13,9 @@ const server = http.createServer((req, res) => {
     // 转换目录
     const catalogUrl = qs.parse(myurl.query).url;
     const host = url.parse(catalogUrl).host;
-    if(config[host]){
-      config[host].catalog(catalogUrl,res);
-    }else{
+    if (config[host]) {
+      config[host].catalog(catalogUrl, res);
+    } else {
       res.statusCode = 404;
       res.setHeader("Content-Type", "text/html;charset=utf-8");
       res.end(`暂时不支持${host}\n`);
@@ -23,17 +24,23 @@ const server = http.createServer((req, res) => {
     // 转换详情
     const infoUrl = qs.parse(myurl.query).url;
     const host = url.parse(infoUrl).host;
-    if(config[host]){
-      config[host].info(infoUrl,res);
-    }else{
+    if (config[host]) {
+      config[host].info(infoUrl, res);
+    } else {
       res.statusCode = 404;
       res.setHeader("Content-Type", "text/html;charset=utf-8");
       res.end(`暂时不支持${host}\n`);
     }
   } else {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World\n");
+    fs.readFile(__dirname+"/index.html", "utf-8", function(err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html;charset=utf-8");
+        res.end(data);
+      }
+    });
   }
 });
 
