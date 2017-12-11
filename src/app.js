0f2218666a03278b2e85ec8cbd64e7466faed958
fan19900404 <125920969@qq.com>
2017-12-11 21:37:04 +0800

    页面详情接口完成

--- src/app.js
+++ src/app.js
@@ -21,9 +21,15 @@ const server = http.createServer((req, res) => {
     }
   } else if (myurl.pathname === "/api/info") {
     // 转换详情
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html;charset=utf-8");
-    res.end(`${myurl.pathname}详情\n`);
+    const infoUrl = qs.parse(myurl.query).url;
+    const host = url.parse(infoUrl).host;
+    if(config[host]){
+      config[host].info(infoUrl,res);
+    }else{
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html;charset=utf-8");
+      res.end(`暂时不支持${host}\n`);
+    }
   } else {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
--- src/config.js
+++ src/config.js
@@ -6,26 +6,52 @@ charset(request);
 module.exports = {
   "www.23xs.cc": {
     charset: "gbk",
-    catalog(url,res){
+    catalog(url, res) {
       request
-      .get(url)
-      .charset("gbk")
-      .end((err, request) => {
-        const body = request.text;
-        const $ = cheerio.load(body);
-        const table = $("table a");
-        const json = [];
-        table.each(function(index,val){
-          json.push({
-            url:'http://www.23xs.cc/'+$(val).attr('href'),
-            title:$(val).text()
-          })
-        })
-        console.log(json)
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/json;charset=utf-8");
-        res.end(`${JSON.stringify(json)}\n`);
-      });
+        .get(url)
+        .charset("gbk")
+        .end((err, request) => {
+          const body = request.text;
+          const $ = cheerio.load(body);
+          const table = $("table a");
+          const json = [];
+          table.each(function(index, val) {
+            json.push({
+              url: "http://www.23xs.cc/" + $(val).attr("href"),
+              title: $(val).text()
+            });
+          });
+          console.log(json);
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/json;charset=utf-8");
+          res.end(`${JSON.stringify(json)}\n`);
+        });
+    },
+    info(url, res) {
+      request
+        .get(url)
+        .charset("gbk")
+        .end((err, request) => {
+          const body = request.text;
+          const $ = cheerio.load(body, { decodeEntities: false });
+          const title = $("#amain h1").text();
+          const content = $("#contents")
+            .html()
+            .split("<br>")
+            .filter((v, i) => {
+              if (v && i) {
+                return true;
+              }
+              return false;
+            })
+            .join("\n")
+            .split("\n")
+            .filter(v => !!v)
+            .map(v => v.trim());
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/json;charset=utf-8");
+          res.end(`${JSON.stringify({ title, content })}\n`);
+        });
     }
   }
 };
